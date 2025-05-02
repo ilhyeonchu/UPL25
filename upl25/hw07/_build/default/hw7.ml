846af486e0aff085e3486659c91a7701d222b6cd
chooilhyeon <168050490+chooilhyeon@users.noreply.github.com>
2025-05-02 10:21:02 +0900

    week9 skel

--- upl25/hw07/_build/default/hw7.ml
+++ upl25/hw07/_build/default/hw7.ml
@@ -14,7 +14,7 @@ let rec interp (ast: Ast.expr) (mem: Store.t) : Store.value =
       interp e2 (Store.add st (interp e1 mem) mem)
     | App (e1, e2) ->
       (match (interp e1 mem, interp e2 mem) with
-        | ClosureV (st, e1, mem'), n2 -> interp e1 (Store.add st n2 mem')  (* C1, C2 는 안되는건가? C2가 파라메터 없다면? 무조건 있어야해서 unit 같은게 있다고 하신거 같긴한데 *)
+        | ClosureV (st, e1, mem'), NumV n2 -> interp e1 (Store.add st (NumV n2) mem')  (* C1, C2 는 안되는건가? C2가 파라메터 없다면? 무조건 있어야해서 unit 같은게 있다고 하신거 같긴한데 *)
         | _, _ -> failwith (Format.asprintf "Not a function: %a" Ast.pp e1))
     | Lambda (st, e2) -> 
         ClosureV (st, e2, mem)
